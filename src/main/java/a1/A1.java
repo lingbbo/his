e7afde3215effd2f34e53979b58af1a77a3ab87d
lingbbo <wutao5361@qq.com>
2020-11-15 11:34:58 +0800

    master01第一次提交

---
+++ src/main/java/a1/A1.java
@@ -0,0 +1,14 @@
+package a1;
+
+/**
+ * @version 1.0
+ * @Classname A1
+ * @Description TODO
+ * @Author wt
+ * @Date 2020/11/15
+ */
+public class A1 {
+    public void a1(){
+        System.out.println("a1");
+    }
+}
