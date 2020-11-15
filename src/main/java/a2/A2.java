04afd2c215e765886a3501eeddf39e70fa74a566
lingbbo <wutao5361@qq.com>
2020-11-15 11:48:26 +0800

    第一次提交

---
+++ src/main/java/a2/A2.java
@@ -0,0 +1,14 @@
+package a2;
+
+/**
+ * @version 1.0
+ * @Classname A2
+ * @Description TODO
+ * @Author wt
+ * @Date 2020/11/15
+ */
+public class A2 {
+    public void a2(){
+        System.out.println("02 第一次提交");
+    }
+}
